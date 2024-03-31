e23f2fa4742d3b06124966f5c32fc7352894386e
Renata Viana <rborgesviana@gmail.com>
2024-03-31 19:17:24 -0300

    adicionando resolução do exercicio-01

--- bootcamp/aula-02/exercicio-01.py
+++ bootcamp/aula-02/exercicio-01.py
@@ -1,4 +1,19 @@
 # #### Inteiros (`int`)
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
+def soma_dois_numeros():
+    try:
+        numero1 = int(input("Digite o primeiro número inteiro: "))
+        numero2 = int(input("Digite o segundo número inteiro: "))
+        soma = numero1 + numero2
+        return soma
+    except ValueError:
+        print("Por favor, insira apenas números inteiros.")
 
+# Essa estrutura garante que o código dentro do bloco if __name__ == "__main__": só será executado quando o arquivo
+# for executado diretamente e não quando for importado como um módulo em outro programa.
+# Isso é útil para organizar e reutilizar código.
+if __name__ == "__main__":
+    resultado = soma_dois_numeros()
+    if resultado is not None:
+        print("A soma dos dois números é: ", resultado)
